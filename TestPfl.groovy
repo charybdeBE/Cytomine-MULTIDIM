7508b5eceacacdb68ae9468aa584804450048795
CharybdeBE <momiamoldava@gmail.com>
2016-12-15 11:36:35 +0100

    Put the characteristic of the image in the begining of the script to change them easily with nano

--- TestPfl.groovy
+++ TestPfl.groovy
@@ -14,8 +14,11 @@ import java.util.concurrent.Future
  */
 
 def size_list = [ 32 ]
-def nr_square = 1
-
+def nr_square = 100
+def local_path = "http://localhost-iip-base/fcgi-bin/iipsrv.fcgi?FIF=/data/28/brug/"
+def local_name = "brug"
+def size_x = 11296
+def size_y = 15653
 
 println "Test 7 // with threadpool"
 
@@ -30,21 +33,23 @@ def benchmark = { closure ->
 
 
 def create_random_pixel = {
-    def xx = random.nextInt(11296)
-    def yy = random.nextInt(15653)
-    new Pxl3(xx, yy)
+    def xx = random.nextInt(size_x)
+    def yy = random.nextInt(size_y)
+    new Pxl3(local_path, local_name, xx, yy)
 }
 
 
 
 class Pxl3 {
-    def path = "http://localhost-iip-base/fcgi-bin/iipsrv.fcgi?FIF=/data/28/brug/" //TODO include it in the constructor
+    def path, name
     int x, y, res
 
-    Pxl3(x, y) {
+    Pxl3(path, name,x, y) {
         this.x = x
         this.y = y
         this.res = 6
+        this.path = path
+        this.name = name
     }
 
     def getSquareInfo(def threadPool, int size) {
@@ -68,7 +73,7 @@ class Pxl3 {
     }
 
     def work = { px, x_end, y ->
-        def txt = px.path + "/brug&PFL=" + px.res + ":" + px.x + "," + y + "-" + x_end + "," + y
+        def txt = px.path + "/" + px.name + "&PFL=" + px.res + ":" + px.x + "," + y + "-" + x_end + "," + y
         // println "Here is from ${txt}"
 
         def texte = new URL(txt).getText()
@@ -87,7 +92,7 @@ size_list.each { size ->
     (1..nr_square).each { tst << create_random_pixel.call() }
     def i = 0
     tst.each { px ->
-        def duration = benchmark{ output << px.getSquareInfo(threadPool, 10) }
+        def duration = benchmark{  px.getSquareInfo(threadPool, 10) }
         println "Test nr ${i} Square Threadpool of size ${size} has taken ${duration} ms  "
         output << duration  + "\n"
         ++i
