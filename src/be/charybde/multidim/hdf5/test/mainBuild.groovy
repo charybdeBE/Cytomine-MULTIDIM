6036e12820a8ca199255651287348172f339a469
CharybdeBE <momiamoldava@gmail.com>
2017-06-14 14:13:25 +0200

    main non cytomine

---
+++ src/be/charybde/multidim/hdf5/test/mainBuild.groovy
@@ -0,0 +1,22 @@
+package be.charybde.multidim.hdf5.test
+
+import be.charybde.multidim.hdf5.input.BuildHyperSpectralFile
+
+/**
+ * Created by laurent on 14.06.17.
+ */
+
+def destination = "/storage/work/multispec/"
+def files = []
+def pattern = "/storage/work/multispec/jpg/brug_chute_anges-cNUMBER-z0-t0.jpg"
+def nrOfFiles = 256
+def burstSize = 100
+for(int i = 1; i < nrOfFiles; ++i){
+    def ff = pattern.replaceAll("NUMBER", i.toString())
+    files << ff
+}
+
+
+
+ BuildHyperSpectralFile h5builder =  new BuildHyperSpectralFile(destination,256, 256, 256, "", files, burstSize);
+ h5builder.createFile(4)
